56b41d2b6cb1b68c7d93e27ef29dc5d4314d5d03
Zeffun <84435005+Zeffun@users.noreply.github.com>
2024-07-21 16:25:20 +0800

    snake movement intervals on key press by updating coordinates

--- app.js
+++ app.js
@@ -3,48 +3,59 @@
 /*---------------------------- Variables (state) ----------------------------*/
 let snakeHeadPosition = [0,0];
 let snakeDirection = "";
+let movementIntervals;
 
 /*------------------------ Cached Element References ------------------------*/
 
 
 /*-------------------------------- Functions --------------------------------*/
+
 function moveSnake(e) {
     const key = e.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
     //when up is pressed:
         //set snakeDirection = "up"
         //while loop for while snakeDirection === "up", the y coordinates increase by 1 at regular intervals
+    
     switch (e.key) {
         case "ArrowLeft":
             // Left pressed
             snakeDirection = "left";
             console.log(snakeDirection);
+            clearInterval(movementIntervals);
+            movementIntervals = setInterval(function() {
+                snakeHeadPosition[0] = snakeHeadPosition[0] - 1;
+                console.log(snakeHeadPosition);
+            }, 500);
             break;
         case "ArrowRight":
             // Right pressed
             snakeDirection = "right";
             console.log(snakeDirection);
-            setInterval(function() {
+            clearInterval(movementIntervals);
+            movementIntervals = setInterval(function() {
                 snakeHeadPosition[0] = snakeHeadPosition[0] + 1;
                 console.log(snakeHeadPosition);
-            }, 1000);
+            }, 500);
             break;
         case "ArrowUp":
             // Up pressed
             snakeDirection = "up";
             console.log(snakeDirection);
-            setInterval(function() {
+            clearInterval(movementIntervals);
+            movementIntervals = setInterval(function() {
                 snakeHeadPosition[1] = snakeHeadPosition[1] + 1;
                 console.log(snakeHeadPosition);
-            }, 1000);
+            }, 500);
             break;
         case "ArrowDown":
             // Down pressed
             snakeDirection = "down";
             console.log(snakeDirection);
-            setInterval(function() {
+            clearInterval(movementIntervals);
+            movementIntervals = setInterval(function() {
                 snakeHeadPosition[1] = snakeHeadPosition[1] - 1;
                 console.log(snakeHeadPosition);
-            }, 1000);
+            }, 500);
             break;
     };
 
@@ -52,7 +63,7 @@ function moveSnake(e) {
     //     setTimeout(function() {
     //         snakeHeadPosition[0] = snakeHeadPosition[0] - 1;
     //         console.log(snakeHeadPosition);
-    //     }, 1000);
+    //     }, 500);
         
     // };
 
